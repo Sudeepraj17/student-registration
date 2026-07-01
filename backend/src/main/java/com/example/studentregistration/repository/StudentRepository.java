655ab91f6b9dbee01185a47f06914586194bd507
Sudeepraj17 <sudeepraj17102004@gmail.com>
2026-07-01 22:41:41 +0530

    Delete backend directory

--- backend/src/main/java/com/example/studentregistration/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentregistration.repository;
-
-import com.example.studentregistration.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
